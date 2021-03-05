3bcea0c65e346608da3ee9dbf5147923ab64651c
nicofreb <nfrebault@hotmail.com>
2021-03-05 16:02:29 +0100

    Mise en place de la première version

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
-import React, { Component } from 'react'
-import shuffle from 'lodash.shuffle'
-
+import { Component } from 'react'
 import './App.css'
-
-import Card from './Card'
-import GuessCount from './GuessCount'
-import HallOfFame, { FAKE_HOF } from './HallOfFame'
-
-const SIDE = 6
-const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+import { BrowserRouter, Route } from "react-router-dom";
+import Accueil from './Accueil/Accueil';
+import Eleves from './Eleve/Eleves';
 
 class App extends Component {
-  cards = this.generateCards()
-
-  generateCards() {
-    const result = []
-    const size = SIDE * SIDE
-    const candidates = shuffle(SYMBOLS)
-    while (result.length < size) {
-      const card = candidates.pop()
-      result.push(card, card)
+    render() {
+        return (
+          <div id="content">
+            <BrowserRouter>
+                <Route exact path="/" component={Accueil} />
+                <Route exact path="/Gestion" component={Eleves} />
+            </BrowserRouter>
+          </div>
+        )
     }
-    return shuffle(result)
-  }
-
-  handleCardClick(card) {
-    console.log(card, 'clicked')
-  }
-
-  render() {
-    const won = new Date().getSeconds() % 2 === 0
-    return (
-      <div className="memory">
-        <GuessCount guesses={0} />
-        {this.cards.map((card, index) => (
-          <Card
-            card={card}
-            feedback="visible"
-            key={index}
-            onClick={this.handleCardClick}
-          />
-        ))}
-        {won && <HallOfFame entries={FAKE_HOF} />}
-      </div>
-    )
-  }
 }
-
 export default App
